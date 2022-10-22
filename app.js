ee0fd8f1f0a2c6123ca417b7474c17ab9c075a45
Alec Hannaford <alec322@gmail.com>
2022-10-22 16:41:18 -0700

    both render methods overwrite the same html element, with the shared readings of min,max,date,pressure

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //https://ashfull1979.blogspot.com/2021/08/mars-weather-and-how-to-fetch-data.html
 
-
 const curiosity_url = 'https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json'
 const perserverance_url = "https://mars.nasa.gov/rss/api/?feed=weather&category=mars2020&feedtype=json"
 
@@ -26,11 +25,18 @@ function perserverance_render(){
         p_recentSol = flipped_sols[sol]
         console.log(p_recentSol)
 
-        const $sol1 = $("#sol1")
-        $sol1.append($("<div>").html("Terrestrial date: " + p_recentSol.terrestrial_date))
+        const $pdate = $("#sol1date")
+        $pdate.html("Terrestrial date of last downlink from Perserverance: " + p_recentSol.terrestrial_date)
 
-        $p = $('#perserverance')
-        $p.html('Max temp: ' + p_recentSol.max_temp)
+        $pmax = $('#sol1max')
+        $pmax.html('Perserverance max temp: ' + p_recentSol.max_temp + ' F°')
+
+        $pmin = $('#sol1min')
+        $pmin.html('Perserverance min temp: ' + p_recentSol.min_temp + ' F°')
+
+        $ppressure = $('#sol1pressure')
+        $ppressure.html('Perserverence atmospheric pressure reading: ' + p_recentSol.pressure)
+
     })
     .catch(error => console.log("Oh no!!", error))
 }
@@ -41,24 +47,27 @@ function curiosity_render(){curiosity_request.then(data =>{
         //const previousSol = data.soles[sol + 1]
  
         //day1
+        console.log(recentSol)
 
         const $rads = $('#rads')
         $rads.html('Radiation level: ' + recentSol.local_uv_irradiance_index)
         if (recentSol.local_uv_irradiance_index === 'High'){
             $rads.css('color','red')
+        } else {
+            $rads.css('color','black')
         }
 
         const $date = $('#sol1date')
-        $date.html('Terrestrial date: ' + recentSol.terrestrial_date)
+        $date.html('Terrestrial date of last downlink from Curiosity: ' + recentSol.terrestrial_date)
 
         const $max = $('#sol1max')
-        $max.html('Max temp: ' + recentSol.max_temp + ' F°')
+        $max.html('Curiosity Max temp: ' + recentSol.max_temp + ' F°')
         
         const $min = $('#sol1min')
-        $min.html('Min temp: ' + recentSol.min_temp + ' F°')
+        $min.html('Curiosity Min temp: ' + recentSol.min_temp + ' F°')
 
-        const $weather = $('#sol1weather')
-        $weather.html('Atomsphere opacity: ' + recentSol.atmo_opacity)
+        const $pressure = $('#sol1pressure')
+        $pressure.html('Curiosity atomspheric pressure reading: ' + recentSol.pressure)
 
         // //day2
         // const $date2 = $('#sol2date')
@@ -80,9 +89,7 @@ function curiosity_render(){curiosity_request.then(data =>{
 
 
 
-// curiosity_render()
-// perserverance_render()
-
+//event listeners
 $("#c_rover_button").click(function(){
     current_rover = "curiosity"
     render_rover()

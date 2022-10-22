f6df436e514d6cd98c4b3e202c0f6d20bfafd7ae
Alec Hannaford <alec322@gmail.com>
2022-10-22 10:54:54 -0700

    scroll back button

--- app.js
+++ app.js
@@ -6,45 +6,48 @@ const url = 'https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=j
 const request = $.ajax(url)
 
 let sol = 0
+
+function render(){request.then(data =>{
+        console.log(data)
+        const recentSol = data.soles[sol]
+        const previousSol = data.soles[sol + 1]
+        console.log(recentSol)
+        //day1
+
+        const $rads = $('#rads')
+        $rads.html('Radiation level: ' + recentSol.local_uv_irradiance_index)
+
+        const $date = $('#sol1date')
+        $date.html('Terrestrial date: ' + recentSol.terrestrial_date)
+
+        const $max = $('#sol1max')
+        $max.html('Max temp: ' + recentSol.max_temp + ' F°')
+        
+        const $min = $('#sol1min')
+        $min.html('Min temp: ' + recentSol.min_temp + ' F°')
+
+        const $weather = $('#sol1weather')
+        $weather.html('Atomsphere opacity: ' + recentSol.atmo_opacity)
+
+        //day2
+        const $date2 = $('#sol2date')
+        $date2.html('Terrestrial date: ' + previousSol.terrestrial_date)
+
+        const $max2 = $('#sol2max')
+        $max2.html('Max temp: ' + previousSol.max_temp + ' F°')
+        
+        const $min2 = $('#sol2min')
+        $min2.html('Min temp: ' + previousSol.min_temp + ' F°')
+
+        const $weather2 = $('#sol2weather')
+        $weather2.html('Atomsphere opacity: ' + previousSol.atmo_opacity)
+
+        })
+    .catch(error => console.log('Failed Request!!!!! ', error))}
+
+render()
+
 $("#scrollback").click(function() {
-    console.log(sol += 1);
-    console.log(sol)
+    sol += 1;
+    render()
 })
-
-request.then(data =>{
-    console.log(data)
-    const recentSol = data.soles[sol]
-    const previousSol = data.soles[sol+1]
-    console.log(recentSol)
-    //day1
-
-    const $rads = $('#rads')
-    $rads.html('Radiation level: ' + recentSol.local_uv_irradiance_index)
-
-    const $date = $('#sol1date')
-    $date.html('Terrestrial date: ' + recentSol.terrestrial_date)
-
-    const $max = $('#sol1max')
-    $max.html('Max temp: ' + recentSol.max_temp + ' F°')
-    
-    const $min = $('#sol1min')
-    $min.html('Min temp: ' + recentSol.min_temp + ' F°')
-
-    const $weather = $('#sol1weather')
-    $weather.html('Atomsphere opacity: ' + recentSol.atmo_opacity)
-
-    //day2
-    const $date2 = $('#sol2date')
-    $date2.html('Terrestrial date: ' + previousSol.terrestrial_date)
-
-    const $max2 = $('#sol2max')
-    $max2.html('Max temp: ' + previousSol.max_temp + ' F°')
-    
-    const $min2 = $('#sol2min')
-    $min2.html('Min temp: ' + previousSol.min_temp + ' F°')
-
-    const $weather2 = $('#sol2weather')
-    $weather2.html('Atomsphere opacity: ' + previousSol.atmo_opacity)
-
-    })
-.catch(error => console.log('Failed Request!!!!! ', error))
